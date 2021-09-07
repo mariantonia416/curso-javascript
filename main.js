45e86cfab96ce63c14d6b9ff15c4e88708d48e05
Mariantonia Castaño <mariantonia416@gmail.com>
2021-09-07 08:58:29 -0500

    Entrega desafío clase 2: Algoritmo con un condicional

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-let age = prompt('Enter the user age');
-let userAge = parseInt(age);
 
-console.log(userAge+6)
+let userName = prompt('Enter your name')
+let userAge = prompt('Enter your age')
+
+if (userAge >= 12) {
+    alert(userName + ', you are over twelve years old, start the game now')
+}
+else if (userAge <= '') {
+    alert(userName + ', you have to enter your age to start the game')
+}
+else  {
+    alert(userName + ', you are under twelve years old, you can not start the game')
+}

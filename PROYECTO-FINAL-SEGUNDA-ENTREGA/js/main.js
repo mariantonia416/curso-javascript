88c970582a45e045f198d7e512fe64b104e57bae
Mariantonia Castaño <mariantonia416@gmail.com>
2021-10-07 23:37:57 -0500

    Segunda entrega del proyecto final

---
+++ PROYECTO-FINAL-SEGUNDA-ENTREGA/js/main.js
@@ -0,0 +1,76 @@
+let producto = [
+    
+    {
+        'codigoItem': '001',
+        'nombreItem': 'Tarima',
+        'precioItem': 1000000
+    },
+    {
+        'codigoItem': '002',
+        'nombreItem': 'Techo',
+        'precioItem': 1200000
+    },{
+        'codigoItem': '003',
+        'nombreItem': 'Pantalla',
+        'precioItem': 3000000        
+    },
+    {
+        'codigoItem': '004',
+        'nombreItem': 'Camara',
+        'precioItem': 800000        
+    },
+    {
+        'codigoItem': '005',
+        'nombreItem': 'Pabellón',
+        'precioItem': 1000000        
+    }
+];
+
+class Servicios {
+    constructor(item, concepto, cantidad) {
+        this.item = item,
+        this.concepto = concepto,
+        this.cantidad = cantidad,
+        this.valor = 0
+    }
+    calculoValorServicio() {
+        
+    }
+}
+
+let cotizacion = [];
+
+function llenarTabla (contenido) {
+    let tabla = document.getElementById('tabla')
+    let fila = '' 
+    contenido.map(item => {
+        fila += `<tr><th scope="row">${item.item}</th><td>${item.concepto}</td><td>${item.cantidad}</td><td>${item.valor}</td></tr>`
+    })
+    tabla.tBodies[0].innerHTML = fila;
+}
+
+function llenarDesplegable (contenido) {
+    let desplegable = document.getElementById('desplegable')
+    let option = '' 
+    contenido.map(item => {
+        option += `<option value="${item.codigoItem}">${item.nombreItem}</option>`
+    })
+    desplegable.innerHTML = option;
+}
+
+
+llenarDesplegable(producto);
+
+let btn = document.getElementById('boton');
+
+btn.onclick = (e) => {
+    e.preventDefault();
+    let dropdown = document.getElementById('desplegable');
+    let quantity = document.getElementById('cantidad');
+    let name = producto.filter(x => x.codigoItem == dropdown.value)[0].nombreItem
+    let tablaProductos = new Servicios(dropdown.value, name, quantity.value);
+    let precio = producto.filter(x => x.codigoItem == dropdown.value)[0].precioItem
+    tablaProductos.valor = precio * quantity.value
+    cotizacion.push (tablaProductos)
+    llenarTabla(cotizacion);
+}

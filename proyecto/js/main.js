1bddad91c457a8548d5fba1a175184126b905801
Mariantonia Castaño <mariantonia416@gmail.com>
2021-10-14 21:47:02 -0500

    Incorporar Jquery al proyecto

--- proyecto/js/main.js
+++ proyecto/js/main.js
@@ -36,41 +36,43 @@ class Servicios {
     calculoValorServicio() {
         
     }
-}
+};
 
 let cotizacion = [];
 
 function llenarTabla (contenido) {
-    let tabla = document.getElementById('tabla')
+    let tabla = $('#tabla')
     let fila = '' 
     contenido.map(item => {
         fila += `<tr><th scope="row">${item.item}</th><td>${item.concepto}</td><td>${item.cantidad}</td><td>${item.valor}</td></tr>`
     })
     tabla.tBodies[0].innerHTML = fila;
-}
+};
 
 function llenarDesplegable (contenido) {
-    let desplegable = document.getElementById('desplegable')
+    let desplegable = $('#desplegable')
     let option = '' 
     contenido.map(item => {
         option += `<option value="${item.codigoItem}">${item.nombreItem}</option>`
     })
     desplegable.innerHTML = option;
-}
+};
 
 
 llenarDesplegable(producto);
 
-let btn = document.getElementById('boton');
+let btn = $('#boton');
 
-btn.onclick = (e) => {
-    e.preventDefault();
-    let dropdown = document.getElementById('desplegable');
-    let quantity = document.getElementById('cantidad');
-    let name = producto.filter(x => x.codigoItem == dropdown.value)[0].nombreItem
-    let tablaProductos = new Servicios(dropdown.value, name, quantity.value);
-    let precio = producto.filter(x => x.codigoItem == dropdown.value)[0].precioItem
-    tablaProductos.valor = precio * quantity.value
-    cotizacion.push (tablaProductos)
-    llenarTabla(cotizacion);
-}
+$(document).ready(function(){
+    btn.click((e) => {
+        e.preventDefault();
+        let dropdown = $('#desplegable');
+        let quantity = $('#cantidad');
+        let name = producto.filter(x => x.codigoItem == dropdown.value)[0].nombreItem;
+        let tablaProductos = new Servicios(dropdown.value, name, quantity.value);
+        let precio = producto.filter(x => x.codigoItem == dropdown.value)[0].precioItem;
+        tablaProductos.valor = precio * quantity.value;
+        cotizacion.push (tablaProductos);
+        llenarTabla(cotizacion);
+    });
+});
